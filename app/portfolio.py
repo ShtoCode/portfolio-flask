9f34e42ede634eb622e27c69dc02a1059d69d6de
germanAstudillo1 <g.astudilloc1@gmail.com>
2023-04-20 04:08:17 -0400

    first commit

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,31 +1,34 @@
-from flask import (Blueprint, render_template,
+from flask import (render_template,
                    request, redirect, url_for, current_app)
 
-import sendgrid
 from sendgrid.helpers.mail import *
+from flask import Flask
+
+app= Flask(__name__)
 
 
-bp = Blueprint('portfolio', __name__, url_prefix='/')
 
 
-@bp.route('/', methods=['GET'])
+
+@app.route('/', methods=['GET'])
 def index():
     return render_template('portfolio/index.html')
 
 
-@bp.route('/mail', methods=['GET', 'POST'])
+@app.route('/mail', methods=['GET', 'POST'])
 def mail():
     nombre = request.form.get('nombre')
     email = request.form.get('email')
     mensaje = request.form.get('mensaje')
 
+    return redirect(url_for('index'))
+"""
     if request.method == 'POST':
         enviar_mail(nombre, email, mensaje)
         return render_template('portfolio/enviar_mail.html')
-        
-    return redirect(url_for('portfolio.index'))
+    """    
 
-
+"""
 def enviar_mail(nombre, email, mensaje):
     mi_email = 'g.astudilloc1@gmail.com'
     sg = sendgrid.SendGridAPIClient(api_key=current_app.config['SENDGRID_KEY'])
@@ -38,11 +41,10 @@ def enviar_mail(nombre, email, mensaje):
     })
 
     html_content = """
-    <p>Hola Germán, tienes un nuevo correo desde tu portafolio:</p>
-    <p>Nombre: -name-</p>
-    <p>Email: -email-</p>
-    <p>Mensaje: -message-</p>
-    """
-
+#    <p>Hola Germán, tienes un nuevo correo desde tu portafolio:</p>
+#    <p>Nombre: -name-</p>
+#    <p>Email: -email-</p>
+#    <p>Mensaje: -message-</p>
+"""
     mail = Mail(mi_email, to_email, "Nuevo contacto desde portafolio", html_content=html_content)
-    response = sg.client.mail.send.post(request_body=mail.get())
+    response = sg.client.mail.send.post(request_body=mail.get())"""
--- app/wsgi.py
+++ app/wsgi.py
@@ -1,13 +1,4 @@
-from flask import Flask
-import os
-
-app= Flask(__name__)
-app.config.from_mapping(
-    SENDGRID_KEY=os.environ.get('SENDGRID_KEY')
-)
-from . import portfolio
-app.register_blueprint(portfolio.bp)
-
+from portfolio import app
 
 if __name__ == '__main__':
     app.run(debug=True)
